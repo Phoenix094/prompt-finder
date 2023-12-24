01e548c236b0fc91ce6568c1273adbbbdc0267e3
Somesh Chavhan <someshchavhan904@gmail.com>
2023-12-24 16:50:17 +0530

    assets and basic app folder created

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+
+
+export const metadata = {
+    title: "Prompt for Finder",
+    desc: "Discover & Share AI Prompts"
+}
+
+const RootLayout = ({ children }) => {
+    return (
+        <html lang="en">
+            <body>
+                <div className="main">
+                    <div className="gradient" />
+                </div>
+                <main className="app">
+                    {children}
+                </main>
+
+            </body>
+        </html>
+    )
+}
+
+export default RootLayout
