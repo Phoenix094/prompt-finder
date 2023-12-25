e337f94f3fe16964fced702354d3b1ea45acbd07
Somesh Chavhan <someshchavhan904@gmail.com>
2023-12-25 19:33:55 +0530

    Feed page is completd

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,64 @@
+"use client"
 
+import { useState, useEffect } from "react"
+
+import PromptCard from "./PromptCard"
+
+
+const PromptCardList = ({ data, handleTagClick }) => {
+    return (
+        <div className='mt-16 prompt_layout'>
+            {data?.map((post) => (
+                <PromptCard
+                    key={post._id}
+                    post={post}
+                    handleTagClick={handleTagClick}
+                />
+            ))}
+        </div>
+    );
+};
 
 const Feed = () => {
+
+    const [searchInput, setSearchInput] = useState("")
+    const [allPost, setAllPost] = useState([])
+
+    const handleSearch = (e) => {
+        setSearchInput(e.target.value)
+    }
+
+    const handleTagClick = async () => {
+
+    }
+
+    const fetchAllPost = async () => {
+        const data = await fetch("/api/prompt")
+        const post = await data.json()
+        setAllPost(post)
+    }
+
+    useEffect(() => {
+        fetchAllPost()
+    }, [])
+
     return (
-        <div>Feed</div>
+        <section className="feed">
+            <form className="w-full relative flex-center">
+                <input
+                    type="text"
+                    value={searchInput}
+                    onChange={handleSearch}
+                    placeholder="search username or tag"
+                    className="search_input peer"
+                />
+
+            </form>
+            <PromptCardList
+                data={allPost}
+                handleTagClick={handleTagClick}
+            />
+        </section>
     )
 }
 
